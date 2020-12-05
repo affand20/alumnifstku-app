70ae3e7ecec3e07a27164001e0703d6c2e429a2e
yagrariksa <luckyrama0@gmail.com>
2020-12-05 13:45:31 +0700

    Splash: coba commit

---
+++ app/src/main/java/id/trydev/alumnifstku/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package id.trydev.alumnifstku.dashboard
+
+import id.trydev.alumnifstku.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
---
+++ app/src/main/java/id/trydev/alumnifstku/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package id.trydev.alumnifstku.splash
+
+import id.trydev.alumnifstku.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
